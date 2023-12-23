6cb3aa3039097670ff4fc96523a5a448bd3656a7
R-S-R-Mounesh <131319795+R-S-R-Mounesh@users.noreply.github.com>
2023-12-23 15:28:44 +0530

    Update DungeonGame.java
    Question3

--- DungeonGame.java
+++ DungeonGame.java
@@ -1,29 +1,48 @@
 public class DungeonGame {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Rows And Colum");
+		int roww = sc.nextInt();
+		int coll = sc.nextInt();
 
-        System.out.println("Enter position of Gold");
-        int row = sc.nextInt();
-        int col = sc.nextInt();
+		char[][] game = new char[roww][coll];
 
-        System.out.println("Enter position of Adventure");
-        int row1 = sc.nextInt();
-        int col1 = sc.nextInt();
-        
+		System.out.println("Enter position of Gold");
+		int row = sc.nextInt();
+		int col = sc.nextInt();
+		game[row][col] = 'G';
 
-        System.out.println("Enter position of Monster");
-        int row2 = sc.nextInt();
-        int col2 = sc.nextInt();
+		System.out.println("Enter position of Adventure");
+		int row1 = sc.nextInt();
+		int col1 = sc.nextInt();
+		game[row1][col1] = 'A';
 
-  
-        int steps = Math.abs(row-row1)+Math.abs(col-col1);
-        int stepsmon=Math.abs(row-row2)+Math.abs(col-col2);
-        if(stepsmon<steps) {
-        System.out.println("Its Imposible");
-        }else
-        	System.out.println(steps);
-       
+		System.out.println("Enter position of Monster");
+		int row2 = sc.nextInt();
+		int col2 = sc.nextInt();
+		game[row2][col2] = 'M';
+
+		int steps = Math.abs(row - row1) + Math.abs(col - col1);
+		int stepsmon = Math.abs(row - row2) + Math.abs(col - col2);
+
+		int j = col1;
+
+		for (int k = 0; k <= steps; k++) {
+			if (j >= steps) {
+				row1--;
+			}
+
+			if (j >= steps) {
+				--j;
+			}
+			System.out.println(row1 + "," + j);
+			j++;
+		}
+
+	}
+}
+
     }
 }
 
