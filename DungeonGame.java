2b3888d796b046bb475bb36c72eb076005276ef2
R-S-R-Mounesh <131319795+R-S-R-Mounesh@users.noreply.github.com>
2023-12-23 13:38:35 +0530

    Update DungeonGame.java
    Question2

--- DungeonGame.java
+++ DungeonGame.java
@@ -10,8 +10,20 @@ public class DungeonGame {
         System.out.println("Enter position of Adventure");
         int row1 = sc.nextInt();
         int col1 = sc.nextInt();
+        
 
+        System.out.println("Enter position of Monster");
+        int row2 = sc.nextInt();
+        int col2 = sc.nextInt();
+
+  
         int steps = Math.abs(row-row1)+Math.abs(col-col1);
-        System.out.println("Shortest Path Is" + steps);
+        int stepsmon=Math.abs(row-row2)+Math.abs(col-col2);
+        if(stepsmon<steps) {
+        System.out.println("Its Imposible");
+        }else
+        	System.out.println(steps);
+       
     }
 }
+
